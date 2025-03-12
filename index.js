120b5e00530968cdee75b1ac3576f15dd1069c9a
Daniele Zeppieri <120616847+dedo-dev@users.noreply.github.com>
2025-03-12 15:37:14 +0000

    checkbox function completed

--- index.js
+++ index.js
@@ -4,80 +4,71 @@ const characters = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
 ]
+let newCharactersArr = [...characters]
+const rangeEl = document.getElementById('range-el')
+const rangeValue = document.getElementById('range-value')
+const numbersEl = document.getElementById('numbers-el')
+const charsetEl = document.getElementById('charset-el')
+const getPasswordBtn = document.getElementById('get-password-btn')
 const passwordOne = document.getElementById('password-one')
 const passwordTwo = document.getElementById('password-two')
-const getPasswordBtn = document.getElementById('get-password-btn')
+let passwordLength = rangeEl.value
 let randomPasswordOne = ''
 let randomPasswordTwo = ''
 
-// INPUT RANGE
-const rangeEl = document.getElementById('range-el')
-const rangeValue = document.getElementById('range-value')
-let passwordLength = rangeEl.value
 rangeEl.addEventListener('input', (e) => {
         rangeValue.textContent = e.target.value
         passwordLength = rangeEl.value
     }
 )
+
 rangeValue.textContent = rangeEl.value
 
-getPasswordBtn.addEventListener('click', () => {
-    getCharacters()
-    randomPasswordOne = ''
-    randomPasswordTwo = ''
-})
+numbersEl.addEventListener('change', remNum)
+charsetEl.addEventListener('change', remCha)
+getPasswordBtn.addEventListener('click', renderPasswords)
 
 function random(arr) {
     return Math.floor(Math.random() * arr.length)
 }
 
+function renderPasswords() {
+    getCharacters()
+    randomPasswordOne = ''
+    randomPasswordTwo = ''
+}
+
+function remNum() {
+    if(numbersEl.checked) {
+        let numbers = '0123456789'
+        newCharactersArr = newCharactersArr.filter(num => !numbers.includes(num))
+    } else {
+        newCharactersArr = [...characters]
+        if(charsetEl.checked) {
+            remCha()
+        }
+    }
+}
+
+function remCha() {
+    if(charsetEl.checked) {
+        let specialCharacters = '!@#$%^&*()_+~`|}{[]\:;?><,./-='
+        newCharactersArr = newCharactersArr.filter(spec => !specialCharacters.includes(spec))
+    } else {
+        newCharactersArr = [...characters]
+        if(numbersEl.checked) {
+            remNum()
+        }
+    }
+}
+
 function getCharacters() {
+    remNum()
+    remCha()
     for(let i = 0; i < passwordLength; i++) {
-        randomPasswordOne += characters[random(characters)]
-        randomPasswordTwo += characters[random(characters)]
+        randomPasswordOne += newCharactersArr[random(newCharactersArr)]
+        randomPasswordTwo += newCharactersArr[random(newCharactersArr)]
     }
     passwordOne.textContent = randomPasswordOne
     passwordTwo.textContent = randomPasswordTwo
 }
-
-function globalForLoop(loop) {
-    for(let i = 0; i < loop.length; i++) {
-        return loop[i]
-        // console.log(loop[i])
-    }
-}
-
-const numbersEl = document.getElementById('numbers-el')
-const charsetEl = document.getElementById('charset-el')
-
-// function removeNum() {
-//     let caratteriDisponibili = [...characters]
-//     if(numbersEl.checked) {
-//         caratteriDisponibili = caratteriDisponibili.filter(num => !'0123456789'.includes(num))
-//     }
-//     console.log(caratteriDisponibili)
-// }
-let password = ''
-function removeNum() {
-    let numbers = '0123456789'
-    let newChartset = [...characters]
-    if(numbersEl.checked) {
-        newChartset = characters.filter(num => !numbers.includes(num))
-    }
-    password = newChartset
-    console.log(newChartset)
-}
-
-
-function removeChart() {
-    let specialChartset = '!@#$%^&*()_+~`|}{[]\:;?><,./-='
-    if(charsetEl.checked) {
-        newChartset = characters.filter(spec => !specialChartset.includes(spec))
-    }
-    password = newChartset
-    console.log(newChartset)
-
-}
-
-numbersEl.addEventListener('change', removeNum)
-charsetEl.addEventListener('change', removeChart)
